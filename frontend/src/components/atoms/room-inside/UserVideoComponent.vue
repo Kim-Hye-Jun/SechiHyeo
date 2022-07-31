872d01d42faabac30514a0a477c259bd45c48533
김혜준 <kimhj0107@naver.com>
2022-07-31 22:54:41 +0900

    feat: add room inside page
    토론방 내부 기본 기능 추가

---
+++ frontend/src/components/atoms/room-inside/UserVideoComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <video autoplay></video>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  components: {},
+  data() {
+    return {
+      example: "",
+    };
+  },
+  methods: {},
+});
+</script>
+
+<style>
+video {
+  background: black;
+}
+</style>
---
+++ frontend/src/components/organisms/room-inside/RoomVideoComponent.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="grid">
+    <user-video-component-vue class="a1"></user-video-component-vue>
+    <user-video-component-vue class="a2"></user-video-component-vue>
+    <user-video-component-vue class="a3"></user-video-component-vue>
+    <user-video-component-vue class="b1"></user-video-component-vue>
+    <user-video-component-vue class="b2"></user-video-component-vue>
+    <user-video-component-vue class="b3"></user-video-component-vue>
+    <div class="c"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import UserVideoComponentVue from "@/components/atoms/room-inside/UserVideoComponent.vue";
+export default defineComponent({
+  components: { UserVideoComponentVue },
+  data() {
+    return {
+      example: "",
+    };
+  },
+  methods: {},
+});
+</script>
+
+<style>
+.grid {
+  background: blue;
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  /* gap: 20px; */
+  grid-template-areas:
+    "a1 c b1"
+    "a2 c b2"
+    "a3 c b3";
+}
+.a1 {
+  width: 100%;
+  grid-area: a1;
+}
+.a2 {
+  width: 100%;
+  grid-area: a2;
+}
+.a3 {
+  width: 100%;
+  grid-area: a3;
+}
+.b1 {
+  width: 100%;
+  grid-area: b1;
+}
+.b2 {
+  width: 100%;
+  grid-area: b2;
+}
+.b3 {
+  width: 100%;
+  grid-area: b3;
+}
+.c {
+  width: 100%;
+  grid-area: c;
+  background: red;
+}
+</style>
