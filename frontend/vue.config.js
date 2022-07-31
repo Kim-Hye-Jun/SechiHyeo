722f5af75fb2b8383d5d4859a0fdec7af2700f00
김혜준 <kimhj0107@naver.com>
2022-07-31 22:34:45 +0900

    chore: add absolute path
    @components 절대경로 추가

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
+  chainWebpack: (config) => {
+    config.resolve.alias.set(
+      "@components",
+      path.resolve(__dirname, "src/components/")
+    );
+  },
   transpileDependencies: true,
 });
