7f47a9afd770f25c07d23103b0cfc6037a80398b
김혜준 <kimhj0107@naver.com>
2022-07-31 22:56:55 +0900

    feat: add router comments
    라우터에 컴포넌트 임포트 주석 추가

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,24 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import TestAtom from "../components/atoms/TestAtom.vue";
+
+// import RoomEntranceSearchInputComponentVue from "@components/atoms/root-entrance/RoomEntranceSearchInputComponent.vue";
+// import RoomEntranceThumbnailComponentVue from "@components/atoms/room-entrance/RoomEntranceThumbnailComponent.vue";
+// import RoomEntranceInfoComponentVue from "@components/atoms/room-entrance/RoomEntranceInfoComponent.vue";
+// import RoomEntranceNameComponentVue from "@components/atoms/room-entrance/RoomEntranceNameComponent.vue";
+// import RoomEntranceComponentVue from "@components/molecules/room-entrance/RoomEntranceComponent.vue";
+// import RoomEntranceBoardComponentVue from "@components/organisms/room-entrance/RoomEntranceBoardComponent.vue";
+// import RoomEntranceSearchInputComponentVue from "@components/atoms/room-entrance/RoomEntranceSearchInputComponent.vue";
+// import RoomEntranceSearchImageComponentVue from "@components/atoms/room-entrance/RoomEntranceSearchImageComponent.vue";
+// import RoomEntrancePageComponentVue from "@components/templates/room-entrance/RoomEntrancePageComponent.vue";
+// import ContainerComponentVue from "@components/atoms/common/ContainerComponent.vue";
+// import UserProfileImageComponentVue from "@components/atoms/common/UserProfileImageComponent.vue";
+// import UserVideoComponentVue from "@components/atoms/room-inside/UserVideoComponent.vue";
+import RoomVideoComponentVue from "@components/organisms/room-inside/RoomVideoComponent.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/test",
-    name: "test",
-    component: TestAtom,
+    path: "/dev",
+    name: "dev",
+    component: RoomVideoComponentVue,
   },
 ];
 
