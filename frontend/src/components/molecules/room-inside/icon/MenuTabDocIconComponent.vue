9aa4e825b55b5571d481f92ca751bae7356a015f
김혜준 <kimhj0107@naver.com>
2022-08-04 00:09:41 +0900

    feat: add room inside menu tab
    토론방 내부 메뉴 탭 기능 구현

---
+++ frontend/src/components/molecules/room-inside/icon/MenuTabDocIconComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg>
+    <menu-tab-svg-component
+      d="M11.9 43q-1.2 0-2.05-.85Q9 41.3 9 40.1V7.9q0-1.2.85-2.05Q10.7 5 11.9 5h17.35L39 14.75V40.1q0 1.2-.85 2.05-.85.85-2.05.85Zm16.25-27.25v-8.5H11.9q-.25 0-.45.2t-.2.45v32.2q0 .25.2.45t.45.2h24.2q.25 0 .45-.2t.2-.45V15.75Zm-16.9-8.5v8.5-8.5V40.75 7.25Z"
+    ></menu-tab-svg-component>
+  </svg>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import MenuTabSvgComponent from "@components/atoms/room-inside/MenuTabSvgPathComponent.vue";
+export default defineComponent({
+  components: { MenuTabSvgComponent },
+  data() {
+    return {
+      example: "",
+    };
+  },
+  methods: {},
+});
+</script>
---
+++ frontend/src/components/organisms/room-inside/MenuTabComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex">
+    <menu-tab-mic-icon-component></menu-tab-mic-icon-component>
+    <menu-tab-cam-icon-component></menu-tab-cam-icon-component>
+    <menu-tab-doc-icon-component></menu-tab-doc-icon-component>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MenuTabMicIconComponent from "@/components/molecules/room-inside/icon/MenuTabMicIconComponent.vue";
+import MenuTabDocIconComponent from "@/components/molecules/room-inside/icon/MenuTabDocIconComponent.vue";
+import MenuTabCamIconComponent from "@/components/molecules/room-inside/icon/MenuTabCamIconComponent.vue";
+
+export default defineComponent({
+  components: {
+    MenuTabMicIconComponent,
+    MenuTabDocIconComponent,
+    MenuTabCamIconComponent,
+  },
+  data() {
+    return {
+      example: "",
+    };
+  },
+  methods: {},
+});
+</script>
+
+<style>
+.flex {
+  background-color: #0e0e23;
+}
+svg {
+  width: 50px;
+  height: 50px;
+  fill: currentColor;
+  color: #9b9ca7;
+  cursor: pointer;
+}
+
+svg active,
+svg:hover {
+  color: #4255d4;
+}
+</style>
