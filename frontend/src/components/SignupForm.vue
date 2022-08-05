efb5b28d755520f1f657e1e4512663fc8d7f8635
hong <hong_1598@naver.com>
2022-08-05 14:52:37 +0900

    feat: Simple Member implements
    가장 기본적인 로그인과 회원가입을 구현했습니다.
    배포 목적으로 디자인과 백엔드와 충돌 가능성이 있는 다른 기능들은
    제거했습니다.

---
+++ frontend/src/components/SignupForm.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="contents">
+    <div class="form-wrapper form-wrapper-sm">
+      <form @submit.stop.prevent class="form">
+        <h2><b>회원 가입</b></h2>
+        <div>
+          <label for="userId">아이디: </label>
+          <input id="userId" type="text" v-model="member.loginId" />
+          <!-- <div v-show="!validId">아이디는 5 ~ 12 글자로 작성해주세요.</div> -->
+        </div>
+        <div>
+          <label for="password">비밀번호: </label>
+          <input id="password" type="text" v-model="member.loginPassword" />
+        </div>
+        <div>
+          <label for="password">비밀번호 확인: </label>
+          <input id="password" type="text" v-model="member.loginPassword2" />
+        </div>
+        <div>
+          <label for="nickname">닉네임: </label>
+          <input id="nickname" type="text" v-model="member.nickname" />
+        </div>
+        <div>
+          <label for="email">이메일: </label>
+          <input id="email" type="text" v-model="member.email" />
+        </div>
+        <div>
+          <label for="phoneNumber">전화번호: </label>
+          <input id="phoneNumber" type="text" v-model="member.phoneNumber" />
+        </div>
+        <button
+          type="button"
+          class="btn"
+          @click="signup"
+          :disabled="isDisabled"
+        >
+          회원 가입
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore, mapState } from "vuex";
+
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    // console.log(store);
+
+    return { store };
+  },
+  data() {
+    return {
+      // form values
+      member: {
+        loginId: "",
+        nickname: "",
+        loginPassword: "",
+        email: "",
+        phoneNumber: "",
+      },
+      loginPassword2: "",
+    };
+  },
+  computed: {
+    // ...mapState(["checkedId", "checkedEmail", "compare_id", "compare_email"]),
+    validId(): boolean {
+      return this.member.loginId.length > 4 && this.member.loginId.length < 13;
+    },
+    validName(): boolean {
+      return this.member.nickname.length > 0;
+    },
+    validPw(): boolean {
+      return (
+        this.member.loginPassword.length > 3 &&
+        this.member.loginPassword.length < 13
+      );
+    },
+    validPw_Re(): boolean {
+      return (
+        // this.member.loginPassword == this.loginPassword2 &&
+        // this.member.loginPassword.length > 0
+        true
+      );
+    },
+    validEmail(): boolean {
+      const exptext = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]+$/;
+      return exptext.test(this.member.email);
+    },
+    // 회원가입 버튼 활성화
+    isDisabled() {
+      if (
+        this.validId &&
+        this.validName &&
+        this.validPw &&
+        this.validPw_Re &&
+        this.validEmail
+      ) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
+  methods: {
+    // idDuplicateCheck() { // 아이디 중복확인용
+    //   this.$store.dispatch("DuplicateId", this.user.userid);
+    // },
+    // emailDuplicateCheck() { // 이메일 중복 확인용
+    //   this.$store.dispatch("DuplicateEmail", this.user.email);
+    // },
+    signup() {
+      // if (!this.checkedId || this.member.userid != this.compare_id) {
+      //   alert("아이디 중복체크를 해주세요");
+      // } else if (!this.checkedEmail || this.member.email != this.compare_email) {
+      //   alert("이메일 중복체크를 해주세요");
+      // } else {
+      this.store.dispatch("memberSignup", this.member);
+      // }
+    },
+  },
+});
+</script>
+<style scoped></style>
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,9 +1,106 @@
 import { createStore } from "vuex";
+import router from "@/router";
+import axios from "axios";
+
+const REST_API = `http://localhost:9999`;
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    username: "",
+    // 로그인 관련
+    isLogin: false, // 로그인 상태
+    memberinfo: {}, // 로그인된 user의 정보
+    // 회원가입 관련
+    checkedId: false, // id 중복체크완료 여부
+    checkedEmail: false, // eamil 중복체크완료 여부
+    compare_id: "", // 중복확인 완료 id값 임시저장
+    compare_email: "", // 중복확인 완료 email값 임시저장
+    // 비밀번호 재설정 관련
+    isAuthPw: false, // 비밀번호 재설정 권한
+    tmp_userid: "", // 임시 저장 아이디
+    // 프로필 재설정 관련
+    isAuthProfile: false, // 프로필 재설정 권한
+  },
+  getters: {
+    isLogin(state) {
+      return state.username !== "";
+    },
+  },
+  mutations: {
+    MEMBER_LOGIN(state, member) {
+      state.isLogin = true;
+      state.memberinfo = member;
+    },
+    DUPL_RESET(state) {
+      state.checkedId = false;
+      state.compare_id = "";
+      state.checkedEmail = false;
+      state.compare_email = "";
+    },
+    MEMBER_LOGOUT(state) {
+      localStorage.removeItem("access-token");
+      state.isLogin = false;
+      state.memberinfo = {};
+    },
+  },
+  actions: {
+    memberLogin({ dispatch }, member) {
+      console.log("로그인 시작");
+      const API_URL = `${REST_API}/register/login`;
+      axios({
+        url: API_URL,
+        method: "POST",
+        params: member,
+      })
+        .then((res) => {
+          if (res.data.message == "success") {
+            console.log(res);
+            localStorage.setItem("access-token", res.data["access-token"]);
+            dispatch("getMemberInfo");
+            // console.log(123456);
+          } else {
+            alert("아이디 또는 비밀번호가 올바르지 않습니다.");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getMemberInfo({ commit }) {
+      const API_URL = `${REST_API}/register/getMember`;
+      const ACESS_TOKEN = localStorage.getItem("access-token");
+      if (ACESS_TOKEN) {
+        axios({
+          url: API_URL,
+          method: "GET",
+          headers: {
+            "access-token": ACESS_TOKEN,
+          },
+        })
+          .then((res) => {
+            commit("MEMBER_LOGIN", res.data);
+            router.push("main");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    },
+    memberSignup({ commit }, member) {
+      const API_URL = `${REST_API}/register/signup`;
+      axios({
+        url: API_URL,
+        method: "POST",
+        params: member,
+      })
+        .then(() => {
+          alert(`${member.nickname}님, 회원가입을 축하드립니다!`);
+          commit("DUPL_RESET");
+          router.push({ name: "memberLogIn" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
 });
