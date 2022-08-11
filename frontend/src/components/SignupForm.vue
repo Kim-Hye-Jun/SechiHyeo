d69dd7ad74f316cf3cc289bb4c151f37749acb5b
hong <hong_1598@naver.com>
2022-08-11 10:24:58 +0900

    feat: Add sign Up Duplicate test
    회원 가입시 중복체크 기능 추가

--- frontend/src/components/SignupForm.vue
+++ frontend/src/components/SignupForm.vue
@@ -6,7 +6,10 @@
         <div>
           <label for="userId">아이디: </label>
           <input id="userId" type="text" v-model="member.loginId" />
-          <!-- <div v-show="!validId">아이디는 5 ~ 12 글자로 작성해주세요.</div> -->
+          <button type="button" @click="idDuplicateCheck">
+            아이디 중복 체크
+          </button>
+          <div>{{ idMessage }}</div>
         </div>
         <div>
           <label for="password">비밀번호: </label>
@@ -15,6 +18,7 @@
         <div>
           <label for="password">비밀번호 확인: </label>
           <input id="password" type="text" v-model="loginPassword2" />
+          <div>{{ pwMessage }}</div>
         </div>
         <div>
           <label for="nickname">닉네임: </label>
@@ -23,15 +27,18 @@
         <div>
           <label for="email">이메일: </label>
           <input id="email" type="text" v-model="member.email" />
+          <button type="button" @click="emailDuplicateCheck">
+            이메일 중복 체크
+          </button>
+          <div>{{ emailMessage }}</div>
         </div>
         <div>
           <label for="phoneNumber">전화번호: </label>
-          <input
-            id="phoneNumber"
-            type="text"
-            v-model="member.phoneNumber"
-            oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');"
-          />
+          <input id="phoneNumber" type="text" v-model="member.phoneNumber" />
+          <button type="button" @click="phoneDuplicateCheck">
+            전화번호 중복 체크
+          </button>
+          <div>{{ phoneMessage }}</div>
         </div>
         <button type="submit" class="btn" :disabled="isDisabled">
           회원 가입
@@ -43,7 +50,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { registerUser } from "@/api/auth";
+import { registerUser, checkUserId, checkEmail, checkPhone } from "@/api/auth";
 
 export default defineComponent({
   data() {
@@ -57,12 +64,9 @@ export default defineComponent({
         phoneNumber: "",
       },
       loginPassword2: "",
-      IdMessage: "",
-      passMessage: "",
-      passCheckMessage: "",
-      nickNameMessage: "",
-      emailMesssage: "",
-      phoneMessage: "",
+      idDuplicate: true,
+      emailDuplicate: true,
+      phoneDuplicate: true,
     };
   },
   computed: {
@@ -85,6 +89,11 @@ export default defineComponent({
       const exptext = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]+$/;
       return exptext.test(this.member.email);
     },
+    validPhone(): boolean {
+      // oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');"
+      const exptext = /^[0-9]+$/;
+      return exptext.test(this.member.phoneNumber);
+    },
     // 회원가입 버튼 활성화
     isDisabled() {
       if (
@@ -92,15 +101,81 @@ export default defineComponent({
         this.validName &&
         this.validPw &&
         this.validPw_Re &&
-        this.validEmail
+        this.validEmail &&
+        this.validPhone &&
+        !this.idDuplicate &&
+        !this.emailDuplicate &&
+        !this.phoneDuplicate
       ) {
         return false;
       } else {
         return true;
       }
     },
+    idMessage() {
+      if (!this.validId) {
+        return "아이디의 길이는 5~12자입니다.";
+      } else if (this.idDuplicate) {
+        return "아이디 중복 체크 해주세요.";
+      } else {
+        return "사용가능한 아이디 입니다.";
+      }
+    },
+    pwMessage() {
+      if (!this.validPw) {
+        return "비밀번호 길이는 4~12자입니다.";
+      } else if (this.validPw_Re) {
+        return "비밀번호가 일치합니다.";
+      } else {
+        return "비밀번호가 일치하지 않습니다.";
+      }
+    },
+    emailMessage() {
+      if (!this.validEmail) {
+        return "이메일 양식을 지켜주세요";
+      } else if (this.emailDuplicate) {
+        return "이메일 중복 체크 해주세요.";
+      } else {
+        return "사용가능한 이메일 입니다.";
+      }
+    },
+    phoneMessage() {
+      if (!this.validPhone) {
+        return "전화번호 양식을 지켜주세요";
+      } else if (this.phoneDuplicate) {
+        return "전화 중복 체크 해주세요.";
+      } else {
+        return "사용가능한 전화번호 입니다.";
+      }
+    },
   },
   methods: {
+    async idDuplicateCheck() {
+      try {
+        const response = await checkUserId(this.member.loginId);
+        this.idDuplicate = false;
+      } catch {
+        alert("이미 사용중인 아이디 입니다!");
+      }
+    },
+    async emailDuplicateCheck() {
+      try {
+        const response = await checkEmail(this.member.email);
+        // console.log(response);
+        this.emailDuplicate = false;
+      } catch {
+        alert("이미 사용중인 이메일 입니다!");
+      }
+    },
+    async phoneDuplicateCheck() {
+      try {
+        const response = await checkPhone(this.member.phoneNumber);
+        // console.log(response);
+        this.phoneDuplicate = false;
+      } catch {
+        alert("이미 사용중인 핸드폰 번호 입니다!");
+      }
+    },
     async signUp() {
       const response = await registerUser(this.member);
       console.log(response);
