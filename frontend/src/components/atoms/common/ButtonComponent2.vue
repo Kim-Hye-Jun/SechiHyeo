813065f30148ce10871c3e1332960759252c0082
김혜준 <kimhj0107@naver.com>
2022-08-12 10:30:53 +0900

    feat: common button design
    공통 버튼 디자인 변경

--- frontend/src/components/atoms/common/ButtonComponent2.vue
+++ frontend/src/components/atoms/common/ButtonComponent2.vue
@@ -1,9 +1,12 @@
 <template>
-  <button class="raise">Raise</button>
+  <button class="button raise">{{ buttonName }}</button>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
+  props: {
+    buttonName: String,
+  },
   components: {},
   data() {
     return {
@@ -15,12 +18,7 @@ export default defineComponent({
 </script>
 
 <style>
-body {
-  background: black;
-}
-
-.raise:hover,
-.raise:focus {
+.raise:hover {
   box-shadow: 0 0.5em 0.5em -0.4em var(--hover);
   transform: translateY(-0.25em);
 }
@@ -30,22 +28,21 @@ body {
   --hover: #0dfbef;
 }
 
-button {
+.button {
+  width: 10em;
   color: var(--color);
   transition: 0.25s;
-}
-button:hover,
-button:focus {
-  border-color: var(--hover);
-  color: #fff;
-}
 
-button {
-  background: none;
+  background: #000000;
   border: 2px solid;
   font: inherit;
   line-height: 1;
   margin: 0.5em;
   padding: 1em 2em;
+}
+.button:hover {
+  cursor: pointer;
+  border-color: var(--hover);
+  color: #fff;
 }
 </style>
