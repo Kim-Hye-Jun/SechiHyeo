3b77e9342dcfc61cbff36d467ec5ab1b6b15db07
김혜준 <kimhj0107@naver.com>
2022-08-12 16:10:55 +0900

    feat: add room-inside drag & drop test code
    드래그 앤 드롭 테스트 작성

--- frontend/src/components/organisms/room-inside/RoomVideoComponent.vue
+++ frontend/src/components/organisms/room-inside/RoomVideoComponent.vue
@@ -5,28 +5,34 @@
       :class="roomAndUserData?.userSideOrder"
     ></user-video-component-vue> -->
     <user-video-component-vue
+      class="a1 video"
       :draggable="isRoomAdmin()"
-      class="a1"
+      value="1"
     ></user-video-component-vue>
     <user-video-component-vue
-      class="a2"
+      class="a2 video"
       :draggable="isRoomAdmin()"
+      value="2"
     ></user-video-component-vue>
     <user-video-component-vue
-      class="a3"
+      class="a3 video"
       :draggable="isRoomAdmin()"
+      value="3"
     ></user-video-component-vue>
     <user-video-component-vue
-      class="b1"
+      class="b1 video"
       :draggable="isRoomAdmin()"
+      value="4"
     ></user-video-component-vue>
     <user-video-component-vue
-      class="b2"
+      class="b2 video"
       :draggable="isRoomAdmin()"
+      value="5"
     ></user-video-component-vue>
     <user-video-component-vue
-      class="b3"
+      class="b3 video"
       :draggable="isRoomAdmin()"
+      value="6"
     ></user-video-component-vue>
     <!-- <user-video
       v-for="sub in subscribers"
@@ -54,13 +60,64 @@ export default defineComponent({
     roomAndUserData: Object,
     userSideOrderMap: Map,
   },
+  data() {
+    return {
+      startElement: null as EventTarget | null,
+    };
+  },
   components: { UserVideoComponentVue },
   methods: {
     isRoomAdmin(): boolean {
       return true;
     },
+    dragOver(e: Event): void {
+      e.preventDefault();
+      console.log("OVER", e);
+    },
+    dragEnter(e: Event): void {
+      e.preventDefault();
+      console.log("ENTER", e);
+    },
+    dragLeave(e: Event): void {
+      e.preventDefault();
+      console.log("LEAVE", e);
+    },
+    dragDrop(e: Event): void {
+      e.preventDefault();
+      console.log("DROP", e.target);
+      console.log("START", this.startElement);
+
+      const dropVideoClassName = (e.target as HTMLDivElement).className;
+      const startVideoClassName = (this.startElement as HTMLDivElement)
+        .className;
+
+      (this.startElement as HTMLDivElement).className = dropVideoClassName;
+      (e.target as HTMLDivElement).className = startVideoClassName;
+    },
+    dragStart(e: Event): void {
+      console.log("START", e.target);
+      this.startElement = e.target;
+    },
+    dragEnd(e: Event): void {
+      e.preventDefault();
+      // console.log("END", e.target);
+      // console.log("START", this.startElement);
+
+      // console.log((e.target as HTMLDivElement).className);
+      // console.log((this.startElement as HTMLDivElement).className);
+    },
   },
-  // setup() {},
+  mounted() {
+    const videos = document.querySelectorAll(".video");
+    for (const video of videos) {
+      video.addEventListener("dragstart", this.dragStart);
+      video.addEventListener("dragover", this.dragOver);
+      video.addEventListener("dragenter", this.dragEnter);
+      video.addEventListener("dragleave", this.dragLeave);
+      video.addEventListener("drop", this.dragDrop);
+      video.addEventListener("dragend", this.dragEnd);
+    }
+  },
 });
 </script>
 
@@ -75,29 +132,33 @@ export default defineComponent({
     "a2 c b2"
     "a3 c b3";
 }
-.a1 {
+.video {
+  padding: 20px;
   width: 100%;
+}
+.a1 {
   grid-area: a1;
+  background: yellow;
 }
 .a2 {
-  width: 100%;
   grid-area: a2;
+  background: red;
 }
 .a3 {
-  width: 100%;
   grid-area: a3;
+  background: blue;
 }
 .b1 {
-  width: 100%;
   grid-area: b1;
+  background: green;
 }
 .b2 {
-  width: 100%;
   grid-area: b2;
+  background: pink;
 }
 .b3 {
-  width: 100%;
   grid-area: b3;
+  background: rgb(142, 133, 88);
 }
 .c {
   width: 100%;
