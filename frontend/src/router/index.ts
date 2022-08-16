8c142062b84dc185bffeb71038159ea1396a4386
김혜준 <kimhj0107@naver.com>
2022-08-17 00:16:05 +0900

    chore: update router, store, configure
    라우터 스토어 타입 설정변경

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,7 @@
 // import RoomEntranceThumbnailComponentVue from "@components/atoms/room-entrance/RoomEntranceThumbnailComponent.vue";
 // import RoomEntranceInfoComponentVue from "@components/atoms/room-entrance/RoomEntranceInfoComponent.vue";
 // import RoomEntranceNameComponentVue from "@components/atoms/room-entrance/RoomEntranceNameComponent.vue";
-// import RoomEntranceComponentVue from "@components/molecules/room-entrance/RoomEntranceComponent.vue";
+import RoomEntranceComponentVue from "@components/molecules/room-entrance/RoomEntranceComponent.vue";
 // import RoomEntranceBoardComponentVue from "@components/organisms/room-entrance/RoomEntranceBoardComponent.vue";
 // import RoomEntranceSearchInputComponentVue from "@components/atoms/room-entrance/RoomEntranceSearchInputComponent.vue";
 // import RoomEntranceSearchImageComponentVue from "@components/atoms/room-entrance/RoomEntranceSearchImageComponent.vue";
@@ -36,11 +36,13 @@ import RoomInsideView from "@components/views/RoomInsideView.vue";
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import store from "@/store/index";
 
+import CommonModal from "@components/atoms/common/CommonModal.vue";
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/test",
     name: "test",
-    component: MenuTabComponent,
+    component: CommonModal,
   },
   {
     path: "/entrance",
